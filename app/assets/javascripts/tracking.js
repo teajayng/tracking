582a10175ef15b89c6ccd08070d3b06ee0ae77de
Terrance <terrance@teajayng.com>
2014-09-16 18:52:05 -0700

    Remove ajaxPrefilter

--- app/assets/javascripts/tracking.js
+++ app/assets/javascripts/tracking.js
@@ -1,6 +1,6 @@
-$.ajaxPrefilter(function(options, originalOptions, jqXHR) {
-  options.url = 'http://localhost:3000' + options.url;
-});
+// $.ajaxPrefilter(function(options, originalOptions, jqXHR) {
+//   options.url = 'http://localhost:3000' + options.url;
+// });
 
 window.Tracking = {
   Models: {},
